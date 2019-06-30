9a9c62d77c9c8f2250db394d2bbe6fe1ab40d125
a015khinntu <b06901075@ntu.edu.tw>
2019-06-30 15:00:56 +0800

    delete produced client

--- api/client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGhost } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faGhost)
-
-ReactDOM.render(<App />, document.getElementById('root'));
